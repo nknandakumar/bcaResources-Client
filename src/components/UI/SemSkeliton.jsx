e4231b538b33e49afd187f3872051e1343012d75
nknandakuamr <tec.thunder2022nk@gmail.com>
2024-10-28 09:18:34 +0530

    skeliton Style is changes

--- src/components/UI/SemSkeliton.jsx
+++ src/components/UI/SemSkeliton.jsx
@@ -1,34 +1,40 @@
-
 const SemSkeliton = () => {
-  return (
-    <div className="  mx-auto px-4  my-10 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 gap-6 justify-center   lg:mx-auto relative   lg:px-32">
-        
-        <div role='status'  className='max-w-sm animate-pulse border-2 flex flex-col justify-center  rounded-xl w-full h-32 lg:h-72  p-5 '>
-   
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[380px] mb-2.5'></p>
-   <p  className='h-5 bg-gray-300 rounded-full max-w-[340px] mb-2.5'></p>
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[320px] mb-2.5'></p>
+	return (
+		<div className="  mx-auto px-4  my-10 grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 gap-6 justify-center   lg:mx-auto relative   lg:px-32">
+			<div role="status" className="flex max-w-sm animate-pulse">
+				<div className="flex-shrink-0">
+					<span className="flex justify-center items-center bg-gray-300 rounded-full w-12 h-12 "></span>
+				</div>
+				<div className="ml-4 mt-2 w-full">
+					<h3 className="h-3 bg-gray-300 rounded-full  w-48 mb-4"></h3>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+				</div>
+			</div>
 
-   </div>
+			<div role="status" className="flex max-w-sm animate-pulse">
+				<div className="flex-shrink-0">
+					<span className="flex justify-center items-center bg-gray-300 rounded-full w-12 h-12 "></span>
+				</div>
+				<div className="ml-4 mt-2 w-full">
+					<h3 className="h-3 bg-gray-300 rounded-full  w-48 mb-4"></h3>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+				</div>
+			</div>
 
-   <div role='status'  className='max-w-sm animate-pulse border-2 flex flex-col justify-center  rounded-xl w-full h-32 lg:h-72  p-5 '>
-   
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[380px] mb-2.5'></p>
-   <p  className='h-5 bg-gray-300 rounded-full max-w-[340px] mb-2.5'></p>
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[320px] mb-2.5'></p>
+			<div role="status" className="flex max-w-sm animate-pulse">
+				<div className="flex-shrink-0">
+					<span className="flex justify-center items-center bg-gray-300 rounded-full w-12 h-12 "></span>
+				</div>
+				<div className="ml-4 mt-2 w-full">
+					<h3 className="h-3 bg-gray-300 rounded-full  w-48 mb-4"></h3>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+					<p className="h-2 bg-gray-300 rounded-full w-[320px] mb-2.5"></p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-   </div>
-
-   <div role='status'  className='max-w-sm animate-pulse border-2 flex flex-col justify-center  rounded-xl w-full h-32 lg:h-72  p-5 '>
-   
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[380px] mb-2.5'></p>
-   <p  className='h-5 bg-gray-300 rounded-full max-w-[340px] mb-2.5'></p>
-   <p  className='h-2 bg-gray-300 rounded-full max-w-[320px] mb-2.5'></p>
-
-   </div>
-
- </div>
-  )
-}
-
-export default SemSkeliton
+export default SemSkeliton;
